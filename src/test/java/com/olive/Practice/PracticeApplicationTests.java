db2361ad55d2f096be558192c51a6225761918f4
bhuvanolive <venkatasaibhuvanesewara.reddy@olivecrypto.com>
2024-11-16 12:56:27 +0530

    Initial commit

---
+++ src/test/java/com/olive/Practice/PracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olive.Practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
